194e71e267d17e0e0fb7d5978a45d21c8ec34fb3
Alejo Rodríguez <alejorod1992@gmail.com>
2022-10-20 09:36:57 -0300

    commit 3

--- src/main/java/com/portfolio/alejor/Controller/PersonaController.java
+++ src/main/java/com/portfolio/alejor/Controller/PersonaController.java
@@ -55,7 +55,7 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
                 }
-   @GetMapping("/personas/traer/perfil/")
+   @GetMapping("/personas/traer/perfil")
     public Persona findPersona() {
      return ipersonaService.findPersona((long)1);
     }
